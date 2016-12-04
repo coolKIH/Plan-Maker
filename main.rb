6e9fbb0b917690c836658968442643407f58bc84
coolKIH <caozihuahh@gmail.com>
2016-12-04 15:51:29 +0800

    debugged

--- main.rb
+++ main.rb
@@ -37,142 +37,234 @@ end
 class Hash
   def getPlanSize
     size = 0
-    self.keys.each do |createdDate|
-      plans = $planFileObj[createdDate]
+    self.keys.each do |planedDate|
+      plans = $planFileObj[planedDate]
       plans.each do |plan|
         size = size + 1
       end
     end
     size
   end
-end
-
-def saveChangesToFile
-  printChDelay 'Saving...'
-  File.open($FILE_PATH, 'w') do |aFile|
-    aFile.puts $planFileObj.to_json
   end
-end
 
 
-def showAll
-  puts 'There are totalling ' + $planFileObj.getPlanSize.to_s + ' items of plans'
-  $planFileObj.keys.each do |createdDate|
-    puts
-    puts 'Your Plans for ' + createdDate
-    numOfPlans = 0
-    plans = $planFileObj[createdDate]
-    plans.each do |plan|
-      numOfPlans = numOfPlans + 1
-      printf("%d: \nDetail: %s\nLocation: %s\nCompanions: %s\nImportant: %s\nPlan Time: %d-%d-%d\n",
-        numOfPlans, plan['detail'], plan['location'], plan['companions'].join(', '), plan['important']?'yes':'no',
-        plan['targetTime']['year'],plan['targetTime']['month'], plan['targetTime']['day'] )
+  def saveChangesToFile
+    printChDelay 'Saving...'
+    File.open($FILE_PATH, 'w') do |aFile|
+      aFile.puts $planFileObj.to_json
     end
   end
-end
-def makeAPlan
-  planEntry = PlanEntry.new
-  printChDelay 'Start planning...'
-  puts 'Please illustrate your plan: what to do?'
-  detail = gets.chomp.strip
-  while detail == ''
-    puts 'Please make sure this field is not empty'
+
+
+  def showAll
+    puts 'There are totalling ' + $planFileObj.getPlanSize.to_s + ' items of plans'
+    $planFileObj.keys.sort.each do |planedDate|
+      puts
+      puts 'Your Plans for ' + planedDate
+      numOfPlans = 0
+      plans = $planFileObj[planedDate]
+      plans.each do |plan|
+        numOfPlans = numOfPlans + 1
+        printf("%d: \nDetail: %s\nLocation: %s\nCompanions: %s\nImportant: %s\nCreated Time: %d-%d-%d\n",
+               numOfPlans, plan['detail'], plan['location'], plan['companions'].join(', '), plan['important']?'yes':'no',
+               plan['targetTime']['year'],plan['targetTime']['month'], plan['targetTime']['day'] )
+      end
+    end
+  end
+
+  def makeAPlan
+    planEntry = PlanEntry.new
+    printChDelay 'Start planning...'
+    puts 'Please illustrate your plan: what to do?'
     detail = gets.chomp.strip
-  end
-  planEntry.detail = detail
-  puts 'Where do you want the event to take place? ' + $skippableMsg
-  planEntry.location = gets.chomp.strip
-  puts 'Who is/are going to do that with you? (comma to split)' + $skippableMsg
-  companions = gets.chomp.strip
-  planEntry.companions = companions.split(/, | ，/)
-  puts 'Important or not? (y for yes, otherwise for no) ' + $skippableMsg
-  ipt = STDIN.getch
-  if ipt == 'y'
-    planEntry.important = true
-  end
-  puts 'Take your time for the plan. How many days from now will it happen? (0 for today) ' + $skippableMsg
-  while daysFromNow = gets.chomp.strip
-    if daysFromNow == ''
-      break
-    elsif daysFromNow.is_number?
-      daysFromNow = daysFromNow.to_i
-      if daysFromNow < 0
-        puts 'Input again. You are not making a plan for yesterday or earlier days'
-      else
-        now = Time.now
-        future = now + 24 * 60 * 60 * daysFromNow
-        planEntry.year = future.year.to_i
-        planEntry.month = future.month.to_i
-        planEntry.day = future.day.to_i
-        planEntry.hour = future.hour.to_i
-        puts 'Fine. You made a plan successfully.'
-        break
+    while detail == ''
+      puts 'Please make sure this field is not empty'
+      detail = gets.chomp.strip
+    end
+    planEntry.detail = detail
+    puts 'Where do you want the event to take place? ' + $skippableMsg
+    planEntry.location = gets.chomp.strip
+    puts 'Who is/are going to do that with you? (comma to split)' + $skippableMsg
+    companions = gets.chomp.strip
+    planEntry.companions = companions.split(/, | ，/)
+    puts 'Important or not? (y for yes, otherwise for no) ' + $skippableMsg
+    ipt = STDIN.getch
+    if ipt == 'y'
+      planEntry.important = true
+    end
+    puts 'which year do you want to make a plan?(2016-3000)'
+    yearToPlan=gets.chomp.strip
+    if yearToPlan!=yearToPlan.to_i.to_s
+      puts 'Input an integer!'
+      return
+    end
+    if yearToPlan.to_i > 3000 || yearToPlan.to_i < 2016
+      puts 'Ensure the range of year 2016-3000'
+      return
+    end
+    puts 'which month?(1-12)'
+    monthToPlan=gets.chomp.strip
+    if monthToPlan!=monthToPlan.to_i.to_s
+      puts 'Input an integer!'
+      return
+    end
+    if monthToPlan.to_i < 1 || monthToPlan.to_i > 12
+      puts 'Ensure the range of month 1-12'
+      return
+    end
+    puts 'What day?'
+    dayToPlan=gets.chomp.strip
+    if dayToPlan!=dayToPlan.to_i.to_s||dayToPlan.to_i < 1
+      puts 'Please input a positive integer!'
+      return
+    end
+    daysOfMonthC=[31,28,31,30,31,30,31,31,30,31,30,31]
+    daysOfMonthL=[31,29,31,30,31,30,31,31,30,31,30,31]
+    if yearToPlan.to_i%4==0
+      if dayToPlan.to_i > daysOfMonthL[monthToPlan.to_i-1]
+        print(yearToPlan,'-',monthToPlan,' does not have ',dayToPlan,' days.')
+        return
       end
     else
-      puts 'Please input a valid number'
+      if dayToPlan.to_i > daysOfMonthC[monthToPlan.to_i-1]
+        print(yearToPlan,'-',monthToPlan,' does not have ',dayToPlan,' days.')
+        return
+      end
     end
-  end
-  printf("Finally, you have made a plan for %d-%d-%d\n",planEntry.year, planEntry.month, planEntry.day)
-  printf("Detail: %s\nLocation: %s\nWith: %s\n", planEntry.detail, planEntry.location, planEntry.companions.join(' and '))
-  puts 'Are you sure for this plan? (y for yes, otherwise for no)'
-  if STDIN.getch == 'y'
-    planObj = {"detail": planEntry.detail, "location": planEntry.location, "companions": planEntry.companions,
-               "important": planEntry.important, "targetTime": {
-            "year": planEntry.year, "month": planEntry.month,
-            "day": planEntry.day, "hour": planEntry.hour
-        }}
+    planEntry.year = yearToPlan.to_i
+    planEntry.month = monthToPlan.to_i
+    planEntry.day = dayToPlan.to_i
+
     createdYear = Time.now.year.to_s
     createdMonth = Time.now.month.to_s
     createdDay = Time.now.day.to_s
     createdTime = [createdYear, createdMonth, createdDay].join '-'
-    $planFileObj[createdTime] = [] unless $planFileObj.has_key?(createdTime)
-    standardJSON = planObj.to_json
-    planObj = JSON.parse standardJSON
-    $planFileObj[createdTime].push planObj
-    puts 'Congratulations! You\'ve got a new plan!'
-    $modifiedFlag = true
-  else
-    puts 'Cancelled'
+    createdCmp = createdYear.to_i*10000+createdMonth.to_i*100+createdDay.to_i
+
+    planedYear = planEntry.year.to_s
+    planedMonth = planEntry.month.to_s
+    planedDay = planEntry.day.to_s
+    planedTime = [planedYear, planedMonth, planedDay].join '-'
+    planedCmp = planedYear.to_i*10000+planedMonth.to_i*100+planedDay.to_i
+    if planedCmp < createdCmp
+      puts 'You are not making a plan for yesterday or earlier days'
+    return
+    end
+    puts 'Fine. You made a plan successfully.'
+    printf("Finally, you have made a plan for %d-%d-%d\n",planEntry.year, planEntry.month, planEntry.day)
+    printf("Detail: %s\nLocation: %s\nWith: %s\n", planEntry.detail, planEntry.location, planEntry.companions.join(' and '))
+    puts 'Are you sure for this plan? (y for yes, otherwise for no)'
+    if STDIN.getch == 'y'
+      planObj = {"detail": planEntry.detail, "location": planEntry.location, "companions": planEntry.companions,
+                 "important": planEntry.important, "targetTime": {
+              "year": Time.now.year, "month": Time.now.month,
+              "day": Time.now.day, "hour": Time.now.hour
+          }
+      }
+      $planFileObj[planedTime] = [] unless $planFileObj.has_key?(planedTime)
+      standardJSON = planObj.to_json
+      planObj = JSON.parse standardJSON
+      $planFileObj[planedTime].push planObj
+      puts 'Congratulations! You\'ve got a new plan!'
+      $modifiedFlag = true
+    else
+      puts 'Cancelled'
+    end
+    puts $planFileObj
   end
-  puts $planFileObj
-end
 
-def tryToDelete numOfitem
+  def tryToDelete(numOfItem,dateToDelete)
+      if !$planFileObj[dateToDelete]
+        puts 'There\'s no plan for this day, failed to delete!'
+      else
+        if $planFileObj[dateToDelete].delete_at(numOfItem.to_i-1)
+          print("A plan for ",dateToDelete," has been deleted!\n")
+          $modifiedFlag=true
+        else
+          print("Plan item not found!\n")
+        end
+        if $planFileObj[dateToDelete].empty?
+          $planFileObj.delete(dateToDelete)
+        end
+      end
+  end
 
-end
-def removeAPlan
-  puts 'These are all the plans that you made'
-  showAll
-  puts 'So which ones you are going to remove?'
-  puts 'Please write down corresponding #\'s of them, using comma to seperate the items'
-  puts 'Enter "quit" to leave here'
-  #Continue here to make changes to the $planFileObj
-  while userInput = gets.chomp.strip.downcase
-    if userInput == 'quit'
-      break
-      elsif userInput.split(',').each do |toDeleteNum|
-        tryToDelete toDeleteNum.strip
+  def removeAPlan
+    puts 'These are all the plans that you made'
+    showAll
+    puts 'The plans of which year do you want to remove?(2016-3000)'
+    yearToDel=gets.chomp.strip
+    if yearToDel!=yearToDel.to_i.to_s
+      puts 'Input an integer!'
+      return
+    end
+    if yearToDel.to_i > 3000 || yearToDel.to_i < 2016
+      puts 'Ensure the range of year 2016-3000'
+      return
+    end
+    puts 'which month?(1-12)'
+    monthToDel=gets.chomp.strip
+    if monthToDel!=monthToDel.to_i.to_s
+      puts 'Input an integer!'
+      return
+    end
+    if monthToDel.to_i < 1 || monthToDel.to_i > 12
+      puts 'Ensure the range of month 1-12'
+      return
+    end
+    puts 'What day?'
+    dayToDel=gets.chomp.strip
+    if dayToDel!=dayToDel.to_i.to_s||dayToDel.to_i < 1
+      puts 'Please input a positive integer!'
+      return
+    end
+    daysOfMonthC=[31,28,31,30,31,30,31,31,30,31,30,31]
+    daysOfMonthL=[31,29,31,30,31,30,31,31,30,31,30,31]
+    if yearToDel.to_i%4==0
+      if dayToDel.to_i > daysOfMonthL[monthToDel.to_i-1]
+        print(yearToDel,'-',monthToDel,' does not have ',dayToDel,' days.')
+        return
+      end
+    else
+      if dayToDel.to_i > daysOfMonthC[monthToDel.to_i-1]
+        print(yearToDel,'-',monthToDel,' does not have ',dayToDel,' days.')
+        return
       end
     end
+    dateToDel=[yearToDel,monthToDel,dayToDel].join '-'
+    puts 'So which ones you are going to remove?'
+    puts 'Please write down corresponding #\'s of them, using comma to seperate the items'
+    numOfPl=gets.chomp.strip
+
+    numOfPl.split(/,|，/).each do |elem|
+      if !elem.is_number?
+        puts "Numbers required"
+        return
+      end
+    end
+
+    numOfPl.split(/,|，/).uniq.sort.reverse.each do |toDeleteNum|
+      tryToDelete(toDeleteNum.strip.chomp, dateToDel)
+    end
   end
-end
-def displayMenu
-  puts
-  itemNum = 1;
-  $menuItems.each do |item|
-    puts itemNum.to_s + '. ' + item
-    itemNum = itemNum + 1
+
+  def displayMenu
+    puts
+    itemNum = 1;
+    $menuItems.each do |item|
+      puts itemNum.to_s + '. ' + item
+      itemNum = itemNum + 1
+    end
+    puts
   end
-  puts
-end
-def printChDelay(str, t=0.01)
-  str.each_char do |ch|
-    sleep t
-    print ch
+  def printChDelay(str, t=0.01)
+    str.each_char do |ch|
+      sleep t
+      print ch
+    end
+    puts
   end
-  puts
-end
-def getUserInput
+  def getUserInput
     displayMenu
     userInput = STDIN.getch
     if userInput=='1'
@@ -200,23 +292,23 @@ def getUserInput
       printChDelay 'Please input a valid number'
     end
     getUserInput
-end
-
-#program starting points
-printChDelay $WELCOME_MESSAGE
-
-if File::exists?($FILE_PATH)
-  planStr = File.read($FILE_PATH)
-  if validJson?planStr
-  $planFileObj = JSON.parse(planStr)
-  else
-    puts 'Currently the file for your plan data is not valid json file.
-    Later the file may be overwritten, but I will back it up for you right now.'
-    FileUtils.copy Pathname, Pathname + '.backup'
   end
 
-else
-  printChDelay $FIRST_TIME_WORDS
-end
+#program starting points
+  printChDelay $WELCOME_MESSAGE
 
-getUserInput
+  if File::exists?($FILE_PATH)
+    planStr = File.read($FILE_PATH)
+    if validJson?planStr
+      $planFileObj = JSON.parse(planStr)
+    else
+      puts 'Currently the file for your plan data is not valid json file.
+    Later the file may be overwritten, but I will back it up for you right now.'
+      FileUtils.copy Pathname, Pathname + '.backup'
+    end
+
+  else
+    printChDelay $FIRST_TIME_WORDS
+  end
+
+  getUserInput
